9bf58c374d14a85f2f406481148981236e1a7968
kyriakos markakis <kyriakos@markakis.me>
2016-12-17 21:13:57 +0000

    Continents insertion script.

---
+++ scripts/continents.sql
@@ -0,0 +1,8 @@
+INSERT INTO `continents` (`code`, `name`) VALUES
+  ('AF', 'Africa'),
+  ('AN', 'Antarctica'),
+  ('AS', 'Asia'),
+  ('EU', 'Europe'),
+  ('NA', 'North America'),
+  ('OC', 'Oceania'),
+  ('SA', 'South America');
